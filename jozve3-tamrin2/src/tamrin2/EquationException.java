00840e081064f5c4042f4d7f49a6ccf86c846542
EmadKashani <100488451+EmadKashani@users.noreply.github.com>
2022-03-12 23:40:16 +0330

    Add files via upload

---
+++ jozve3-tamrin2/src/tamrin2/EquationException.java
@@ -0,0 +1,9 @@
+package tamrin2;
+
+public class EquationException extends Exception{
+
+    public EquationException(String deltaCheck){
+        super(deltaCheck);
+    }
+    public EquationException(){}
+}
